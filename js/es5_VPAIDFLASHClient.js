80efaf8417e1b98b329c0eed9b3e97da2eaf66f1
Adam Tyler <lazy9669@gmail.com>
2018-06-14 12:42:34 -0500

    Adding es5 compiled version and making entry. Circumvents issue in browserified code

---
+++ js/es5_VPAIDFLASHClient.js
@@ -0,0 +1,196 @@
+'use strict';
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var swfobject = require('swfobject');
+
+var JSFlashBridge = require('./jsFlashBridge').JSFlashBridge;
+var VPAIDAdUnit = require('./VPAIDAdUnit').VPAIDAdUnit;
+
+var noop = require('./utils').noop;
+var callbackTimeout = require('./utils').callbackTimeout;
+var isPositiveInt = require('./utils').isPositiveInt;
+var createElementWithID = require('./utils').createElementWithID;
+var uniqueVPAID = require('./utils').unique('vpaid');
+var createFlashTester = require('./flashTester.js').createFlashTester;
+
+var ERROR = 'error';
+var FLASH_VERSION = '10.1.0';
+
+var flashTester = { isSupported: function isSupported() {
+        return true;
+    } }; // if the runFlashTest is not run the flashTester will always return true
+
+var VPAIDFLASHClient = function () {
+    function VPAIDFLASHClient(vpaidParentEl, callback) {
+        var swfConfig = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : { data: 'VPAIDFlash.swf', width: 800, height: 400 };
+
+        var _this = this;
+
+        var params = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : { wmode: 'transparent', salign: 'tl', align: 'left', allowScriptAccess: 'always', scale: 'noScale', allowFullScreen: 'true', quality: 'high' };
+        var vpaidOptions = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : { debug: false, timeout: 10000 };
+
+        _classCallCheck(this, VPAIDFLASHClient);
+
+        var me = this;
+
+        this._vpaidParentEl = vpaidParentEl;
+        this._flashID = uniqueVPAID();
+        this._destroyed = false;
+        callback = callback || noop;
+
+        swfConfig.width = isPositiveInt(swfConfig.width, 800);
+        swfConfig.height = isPositiveInt(swfConfig.height, 400);
+
+        createElementWithID(vpaidParentEl, this._flashID, true);
+
+        params.movie = swfConfig.data;
+        params.FlashVars = 'flashid=' + this._flashID + '&handler=' + JSFlashBridge.VPAID_FLASH_HANDLER + '&debug=' + vpaidOptions.debug + '&salign=' + params.salign;
+
+        if (!VPAIDFLASHClient.isSupported()) {
+            return onError('user don\'t support flash or doesn\'t have the minimum required version of flash ' + FLASH_VERSION);
+        }
+
+        this.el = swfobject.createSWF(swfConfig, params, this._flashID);
+
+        if (!this.el) {
+            return onError('swfobject failed to create object in element');
+        }
+
+        var handler = callbackTimeout(vpaidOptions.timeout, function (err, data) {
+            $loadPendedAdUnit.call(_this);
+            callback(err, data);
+        }, function () {
+            callback('vpaid flash load timeout ' + vpaidOptions.timeout);
+        });
+
+        this._flash = new JSFlashBridge(this.el, swfConfig.data, this._flashID, swfConfig.width, swfConfig.height, handler);
+
+        function onError(error) {
+            setTimeout(function () {
+                callback(new Error(error));
+            }, 0);
+            return me;
+        }
+    }
+
+    _createClass(VPAIDFLASHClient, [{
+        key: 'destroy',
+        value: function destroy() {
+            this._destroyAdUnit();
+
+            if (this._flash) {
+                this._flash.destroy();
+                this._flash = null;
+            }
+            this.el = null;
+            this._destroyed = true;
+        }
+    }, {
+        key: 'isDestroyed',
+        value: function isDestroyed() {
+            return this._destroyed;
+        }
+    }, {
+        key: '_destroyAdUnit',
+        value: function _destroyAdUnit() {
+            delete this._loadLater;
+
+            if (this._adUnitLoad) {
+                this._adUnitLoad = null;
+                this._flash.removeCallback(this._adUnitLoad);
+            }
+
+            if (this._adUnit) {
+                this._adUnit._destroy();
+                this._adUnit = null;
+            }
+        }
+    }, {
+        key: 'loadAdUnit',
+        value: function loadAdUnit(adURL, callback) {
+            var _this2 = this;
+
+            $throwIfDestroyed.call(this);
+
+            if (this._adUnit) {
+                this._destroyAdUnit();
+            }
+
+            if (this._flash.isReady()) {
+                this._adUnitLoad = function (err, message) {
+                    if (!err) {
+                        _this2._adUnit = new VPAIDAdUnit(_this2._flash);
+                    }
+                    _this2._adUnitLoad = null;
+                    callback(err, _this2._adUnit);
+                };
+
+                this._flash.callFlashMethod('loadAdUnit', [adURL], this._adUnitLoad);
+            } else {
+                this._loadLater = { url: adURL, callback: callback };
+            }
+        }
+    }, {
+        key: 'unloadAdUnit',
+        value: function unloadAdUnit() {
+            var callback = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : undefined;
+
+            $throwIfDestroyed.call(this);
+
+            this._destroyAdUnit();
+            this._flash.callFlashMethod('unloadAdUnit', [], callback);
+        }
+    }, {
+        key: 'getFlashID',
+        value: function getFlashID() {
+            $throwIfDestroyed.call(this);
+            return this._flash.getFlashID();
+        }
+    }, {
+        key: 'getFlashURL',
+        value: function getFlashURL() {
+            $throwIfDestroyed.call(this);
+            return this._flash.getFlashURL();
+        }
+    }]);
+
+    return VPAIDFLASHClient;
+}();
+
+setStaticProperty('isSupported', function () {
+    return swfobject.hasFlashPlayerVersion(FLASH_VERSION) && flashTester.isSupported();
+}, true);
+
+setStaticProperty('runFlashTest', function (swfConfig) {
+    flashTester = createFlashTester(document.body, swfConfig);
+});
+
+function $throwIfDestroyed() {
+    if (this._destroyed) {
+        throw new Error('VPAIDFlashToJS is destroyed!');
+    }
+}
+
+function $loadPendedAdUnit() {
+    if (this._loadLater) {
+        this.loadAdUnit(this._loadLater.url, this._loadLater.callback);
+        delete this._loadLater;
+    }
+}
+
+function setStaticProperty(propertyName, value) {
+    var writable = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
+
+    Object.defineProperty(VPAIDFLASHClient, propertyName, {
+        writable: writable,
+        configurable: false,
+        value: value
+    });
+}
+
+VPAIDFLASHClient.swfobject = swfobject;
+
+module.exports = VPAIDFLASHClient;
